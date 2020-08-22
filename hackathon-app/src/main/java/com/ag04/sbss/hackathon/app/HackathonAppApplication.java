4c8d9388e63b72bac2afbb206ca7f6a36112a1a6
Ivan Munic <ivan.munic@ag04.com>
2020-08-22 02:31:53 +0200

    Add hackaton-app skeleton

---
+++ hackathon-app/src/main/java/com/ag04/sbss/hackathon/app/HackathonAppApplication.java
@@ -0,0 +1,13 @@
+package com.ag04.sbss.hackathon.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackathonAppApplication.class, args);
+	}
+
+}
---
+++ hackathon-app/src/test/java/com/ag04/sbss/hackathon/app/HackathonAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ag04.sbss.hackathon.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
